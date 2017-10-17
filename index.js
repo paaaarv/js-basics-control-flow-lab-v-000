c2ca97136d84fa6ed84eff64f44dcdd04928c7a5
Parv Kumar <parvathikumar92@gmail.com>
2017-10-17 23:12:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,4 +16,18 @@ function scuberGreetingForFeet(feet) {
 
 function ternaryCheckCity(city){
   city === "NYC" ?  "Ok, sounds good" :  "No go.";
+};
+
+
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous': 
+      return "Thank you so much."
+      break; 
+    case 'not as generous': 
+      return "Thank you."; 
+      break; 
+    default: 
+      return "Bye."
+  }
 }
