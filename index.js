9ef004ee41392a2b1d8f2619cd357ad77d7d933a
Parv Kumar <parvathikumar92@gmail.com>
2017-10-17 22:49:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // Write your code in this file!
 
 
-let scuberGreetingForFeet; 
 
-function scuberGreetingForFeet(feet) { 
-  if (feet <=400){ 
-    scuberGreetingForFeet = "This one's on me!"
+function scuberGreetingForFeet(feet) {
+  if (feet <=400){
+    return "This one's on me!"
   }else if ()
 
 }
