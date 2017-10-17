98a1de96bcf7849a8d9be6fcaa2f5ff68e1f081c
Parv Kumar <parvathikumar92@gmail.com>
2017-10-17 23:24:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your code in this file!
+
+
+
+function scuberGreetingForFeet(feet) {
+  if (feet <=400){
+    return "This one is on me!"
+  }else if(feet > 2500){
+    return "No can do."
+  }else if(feet > 2000) {
+    return "I will gladly take your thirty bucks."
+  }
+
+};
+function ternaryCheckCity(theCity){
+  const nyc = "NYC"
+  const value = (theCity === nyc) ?  `Ok, sounds good.` : `No go.`
+
+  value;
+
+  if (theCity=== nyc){
+    return "Ok, sounds good."
+  }else {
+    return "No go."
+  }
+
+};
+
+
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return "Thank you so much."
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    default:
+      return "Bye."
+  }
+}
