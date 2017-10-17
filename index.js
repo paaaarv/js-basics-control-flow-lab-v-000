1d1b24354eba056cf8e9d771a58e49db7c922f57
Parv Kumar <parvathikumar92@gmail.com>
2017-10-17 22:54:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,15 @@
 function scuberGreetingForFeet(feet) {
   if (feet <=400){
     return "This one is on me!"
+  }else if(feet > 2500){
+    return "No can do."
+  }else if(feet > 2000) {
+    return "I will gladly take your thirty bucks."
   }
 
+}
+
+
+function ternaryCheckCity(city){
+  city === "NYC" ? "Ok, sounds good" : "No go."
 }
